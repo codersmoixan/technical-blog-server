17a194cbbb38cf7fe7a07eeb38d46a4e22f765c2
zhengji.su <367265893@qq.com>
2022-12-01 01:51:37 +0800

    feat: [TB_web] 完成PC的editor页面的布局

--- web/src/components/private/Editor/ShareEditor.tsx
+++ web/src/components/private/Editor/ShareEditor.tsx
@@ -10,24 +10,58 @@ import { makeStyles } from "@mui/styles";
 import type { Theme } from "@mui/material";
 
 import '@wangeditor/editor/dist/css/style.css'
-import clsx from "clsx";
-
-interface ShareEditorProps {
-  onChange?: (editor: IDomEditor) => void;
-  className?: string;
-}
+import Root from "components/common/Layout/Root";
+import Box from "@mui/material/Box";
+import Buttons from "components/common/Buttons";
 
 const useStyles = makeStyles((theme: Theme) => ({
-  root: {},
-  toolbar: {
+  root: {
+    minHeight: '100vh',
+    backgroundColor: theme.status.white,
+    borderLeft: `1px solid ${theme.status.colorSecondary}`,
+    borderRight: `1px solid ${theme.status.colorSecondary}`,
+  },
+  header: {
     position: 'sticky',
     top: 0,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: theme.spacing( 0, 3),
+    height: 85,
+    backgroundColor: theme.status.white,
     zIndex: 999,
-    borderBottom: `1px solid ${theme.status.colorSecondary}`
+  },
+  title: {
+    width: '70%',
+    height: 49,
+    fontWeight: 700,
+    fontSize: 24,
+    outline: 'none',
+    border: 'none',
+  },
+  actions: {
+    display: 'flex'
+  },
+  submit: {
+    marginLeft: theme.spacing(3),
+  },
+  editorContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+  },
+  toolbar: {
+    position: 'sticky',
+    top: 85,
+    zIndex: 999,
+    borderBottom: `1px solid ${theme.status.colorSecondary}`,
+    borderTop: `1px solid ${theme.status.colorSecondary}`,
   },
   editor: {
-    height: '100%'
-  }
+    minHeight: '800px',
+    flex: 1,
+  },
 }))
 
 const excludeToolKey = [
@@ -35,10 +69,11 @@ const excludeToolKey = [
   'numberedList', 'group-justify', 'group-indent', 'group-video', 'insertTable', 'todo'
 ]
 
-function ShareEditor({ onChange, className, ...other }: ShareEditorProps) {
+function ShareEditor() {
   const classes = useStyles()
 
   const [editor, setEditor] = useState<IDomEditor | null>(null)
+  const [html, setHtml] = useState('')
 
   const toolbarConfig: Partial<IToolbarConfig> = {
 
@@ -51,7 +86,7 @@ function ShareEditor({ onChange, className, ...other }: ShareEditorProps) {
       // 编辑器创建之后，记录 editor 实例，重要 ！！！ （有了 editor 实例，就可以执行 editor API）
       setEditor(editor)
     },
-    onChange: (editor: IDomEditor) => onChange?.(editor)
+    onChange: (editor: IDomEditor) => setHtml(editor.getHtml())
   }
 
   // 组件销毁时，及时销毁 editor 实例，重要！！！
@@ -74,17 +109,26 @@ function ShareEditor({ onChange, className, ...other }: ShareEditorProps) {
   }, [editor])
 
   return (
-    <div className={clsx(className, classes.root)} {...other}>
-      <Toolbar
-        editor={editor}
-        defaultConfig={toolbarConfig}
-        className={classes.toolbar}
-      />
-      <Editor
-        defaultConfig={editorConfig}
-        className={classes.editor}
-      />
-    </div>
+    <Root className={classes.root}>
+      <Box className={classes.header}>
+        <input placeholder="请输入文章标题..." className={classes.title} />
+        <Box className={classes.actions}>
+          <Buttons variant="outlined">草稿箱</Buttons>
+          <Buttons variant="contained" className={classes.submit}>发布</Buttons>
+        </Box>
+      </Box>
+      <Box className={classes.editorContainer}>
+        <Toolbar
+          editor={editor}
+          defaultConfig={toolbarConfig}
+          className={classes.toolbar}
+        />
+        <Editor
+          defaultConfig={editorConfig}
+          className={classes.editor}
+        />
+      </Box>
+    </Root>
   )
 }
 
