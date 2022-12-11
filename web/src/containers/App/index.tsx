88916ef630c9cdc5a17a8ba2b0f9d076d4e856eb
zhengji.su <367265893@qq.com>
2022-12-11 18:25:34 +0800

    feat: [TB_web] 新增App组件作为根容器

---
+++ web/src/containers/App/index.tsx
@@ -0,0 +1,51 @@
+/**
+ * @author zhengji.su
+ * @description App
+ */
+
+import Box from '@mui/material/Box';
+import { motion, useScroll } from "framer-motion";
+import { makeStyles } from "@mui/styles";
+import NodeVisible from "components/NodeVisible";
+import Navigation from "components/Navigation";
+import BasicSpeedDial from "components/BasicSpeedDial";
+import Footer from "components/Footer";
+import type { AppProps } from "next/app";
+import type { Theme } from "@mui/material";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  scrollProgress: {
+    position: 'fixed',
+    top: 0,
+    width: '100%',
+    height: 2,
+    transformOrigin: '0%',
+    backgroundColor: theme.palette.primary.main,
+    zIndex: 9999,
+  }
+}))
+
+function App({ Component, pageProps }: AppProps) {
+  const classes = useStyles()
+  const { scrollYProgress } = useScroll()
+
+  return (
+    <Box>
+      <motion.div style={{ scaleX: scrollYProgress }} className={classes.scrollProgress} />
+      <NodeVisible>
+        <Navigation />
+      </NodeVisible>
+      <Box position="relative" zIndex={9}>
+        <Component {...pageProps} />
+        <NodeVisible>
+          <BasicSpeedDial />
+        </NodeVisible>
+      </Box>
+      <NodeVisible>
+        <Footer />
+      </NodeVisible>
+    </Box>
+  )
+}
+
+export default App
---
+++ web/src/containers/Home/index.tsx
@@ -0,0 +1,73 @@
+import { makeStyles } from "@mui/styles";
+import Content from "components/Layout/Content";
+import type { Theme } from "@mui/material";
+import Video from "components/../containers/Home/components/Video";
+import Typography from "@mui/material/Typography";
+import Buttons from "components/Buttons";
+import {useRouter} from "next/router";
+import routes from "@/src/routes";
+import Root from "components/Layout/Root";
+import Banner from "components/Layout/Banner";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: "center",
+    padding: theme.spacing(0, 3),
+  },
+  banner: {
+    justifyContent: 'flex-end',
+    width: 484,
+    [theme.breakpoints.down('md')]: {
+      marginTop: theme.spacing(3),
+      justifyContent: 'flex-start',
+      width: 'auto',
+    }
+  },
+  subtitle: {
+    margin: theme.spacing(3, 0)
+  },
+  startShare: {
+    width: 195,
+    height: 50,
+    fontSize: 16
+  }
+}))
+
+function Home() {
+  const classes = useStyles()
+  const history = useRouter()
+
+  const handleToShare = () => history.push(routes.share())
+
+  return (
+    <Root position="relative">
+      <Content className={classes.root}>
+        <Banner className={classes.banner}>
+          <Typography variant="h2" fontWeight={400}>
+            你一路颠沛流离
+          </Typography>
+          <Typography variant="h2" fontWeight={400}>
+            到最遥远的地方旅行
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            classes={{ root: classes.subtitle }}
+          >
+            未来，就是你站在茫茫大海的这一边，遥望着海的那一边，充满好奇心，憧憬着对海那边的向往，正是对未知的不了解和向往，所以才有了去追逐未来的勇气。
+          </Typography>
+          <Buttons
+            variant="contained"
+            className={classes.startShare}
+            onClick={handleToShare}
+          >
+            开始你的旅行
+          </Buttons>
+        </Banner>
+      </Content>
+      <Video />
+    </Root>
+  )
+}
+
+export default Home
