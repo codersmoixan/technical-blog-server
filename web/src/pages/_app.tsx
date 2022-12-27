c76e8dbedd0f22c9ef414e09a80b9a7801853bc3
zhengji.su <367265893@qq.com>
2022-12-27 15:10:13 +0800

    feat: [TB_web] 初始化ReactQuery

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@mui/material/styles";
 import { SnackbarProvider } from "notistack"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import SetupApp from "containers/App"
 
 import theme from "@/src/theme"
@@ -10,14 +13,18 @@ import "../assets/common.css"
 import type { AppProps } from 'next/app'
 
 export default function App(props: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
 
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <SnackbarProvider>
-          <SetupApp {...props} />
-        </SnackbarProvider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <SnackbarProvider>
+            <SetupApp {...props} />
+          </SnackbarProvider>
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
     </Provider>
   )
 }
