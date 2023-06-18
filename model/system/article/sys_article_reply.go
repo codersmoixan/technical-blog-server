723045513453fa01d79100484ae4ff9b44688646
codersmoixan <367265893@qq.com>
2023-06-18 23:08:49 +0800

    feat: 优化回复和评论的字段

--- model/system/article/sys_article_reply.go
+++ model/system/article/sys_article_reply.go
@@ -6,12 +6,13 @@ type SysArticleReply struct {
 	global.TB_MODEL
 	ReplyId string `json:"replyId" gorm:"comment:回复id"`
 	ArticleId string `json:"articleId" gorm:"comment:文章id"`
-	ReplyContent string `json:"replyContent" gorm:"comment:回复内容"`
+	ReplyContent string `json:"content" gorm:"comment:回复内容"`
 	ReplyCommentId string `json:"replyCommentId" gorm:"comment:被回复的评论的id"`
 	ReplyToReplyId string `json:"replyToReplyId" gorm:"comment:被回复的回复id"`
 	ReplyToUserId string `json:"replyToUserId" gorm:"comment:被回复的用户id"`
 	ReplyUserId string `json:"replyUserId" gorm:"comment:回复者id"`
-	ReplyLiked int `json:"replyLiked" gorm:"comment:被点赞到的数量;default:0"`
+	ReplyLiked int `json:"liked" gorm:"comment:被点赞到的数量;default:0"`
+	ReplyCount int `json:"replyCount" gorm:"comment:被回复的数量;default:0"`
 }
 
 func (SysArticleReply) TableName() string {
